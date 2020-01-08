e315ae31065ebcb3fbce547c5fca63e94ba26d66
HalaSaadeh <halagsaadeh@gmail.com>
2020-01-08 20:34:15 +0200

    scnd commit:

---
+++ parsingjsonfromweb.py
@@ -0,0 +1,41 @@
+import urllib.request, urllib.parse, urllib.error
+import ssl
+import json
+api_key = False
+# If you have a Google Places API key, enter it here
+# api_key = 'AIzaSy___IDByT70'
+# https://developers.google.com/maps/documentation/geocoding/intro
+
+if api_key is False:
+    api_key = 42
+    serviceurl = 'http://py4e-data.dr-chuck.net/xml?'
+else :
+    serviceurl = 'https://maps.googleapis.com/maps/api/geocode/xml?'
+
+# Ignore SSL certificate errors
+ctx = ssl.create_default_context()
+ctx.check_hostname = False
+ctx.verify_mode = ssl.CERT_NONE
+
+while True:
+    address = input('Enter location: ')
+    if len(address) < 1: break
+
+    parms = dict()
+    parms['address'] = address
+    if api_key is not False: parms['key'] = api_key
+    url = address
+    print('Retrieving', url)
+    uh = urllib.request.urlopen(url, context=ctx)
+
+    data = uh.read()
+    print('Retrieved', len(data), 'characters')
+    print(data.decode())
+
+    info = json.loads(data)
+    sum=0
+    print("Count",len(info["comments"]))
+    for comment in info["comments"]:
+        sum+=int(comment['count'])
+
+    print(sum)
